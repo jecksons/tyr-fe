308ef92c29bb37afc3e8c6654e3993b87f9e05b0
jecksons <jeckson.es@gmail.com>
2021-04-10 08:40:05 -0300

    Added debug info

--- src/components/pages/NotFound/index.js
+++ src/components/pages/NotFound/index.js
@@ -1,9 +1,11 @@
 import './notfound.css';
+import Config from '../../../services/config';
 
 export default function NotFound() {
     return (
         <div id="notfound">
             <strong id="message">Ooops! A página não existe!</strong>
+            <p>{Config().apiURL}</p>
         </div>
     )
 }
